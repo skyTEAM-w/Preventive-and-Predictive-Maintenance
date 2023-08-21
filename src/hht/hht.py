be55125a16fc74c1c6f292c167ce40e818937d32
WuChengpei_sky <w2563070231@outlook.com>
2023-08-21 10:40:00 +0800

    HHT能量数据处理

--- src/hht/hht.py
+++ src/hht/hht.py
@@ -74,7 +74,7 @@ def hht_picture(imfs, imfs_ht, time, size):
     pass
 
 
-def feature_processing(imfs_ht=np.ndarray) -> np.ndarray:
+def feature_processing_inst_freq(imfs_ht=np.ndarray) -> np.ndarray:
     n_component = imfs_ht.shape[0]
     frequency_array = []
     for i in range(n_component):
@@ -85,3 +85,56 @@ def feature_processing(imfs_ht=np.ndarray) -> np.ndarray:
         pass
     return np.array(frequency_array)
     pass
+
+
+def feature_processing_energy(imfs=np.ndarray, imfs_ht=np.ndarray, fs=int, size=int) -> float:
+    n_component = imfs.shape[0]
+    imfs_ht = imfs_ht[:-1]
+
+    for i in range(imfs_ht.shape[0]):
+
+        pass
+
+    pass
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from scipy.signal import hilbert
+# from PyEMD import EMD
+#
+# # 生成示例信号（可以替换为你自己的信号数据）
+# sample_rate = 20480
+# time = np.linspace(0, 1, sample_rate)
+# frequency1 = 5
+# frequency2 = 50
+# signal = np.sin(2 * np.pi * frequency1 * time) + np.sin(2 * np.pi * frequency2 * time)
+#
+# # 进行经验模态分解（EMD）
+# emd = EMD()
+# imf = emd(signal)
+# imf = imf[:-1]  # 去除最后一个 imf
+#
+# # 初始化时间频率幅度谱
+# time_freq_amp_spectrum = []
+#
+# # 遍历每个 IMF
+# for i in range(len(imf)):
+#     analytic_signal = hilbert(imf[i])  # 计算解析信号
+#     instantaneous_frequency = np.diff(np.unwrap(np.angle(analytic_signal)))  # 计算瞬时频率
+#
+#     time_points = np.arange(len(imf[i]))  # 时间点
+#     freq_points = instantaneous_frequency * (sample_rate / (2 * np.pi))  # 瞬时频率转换为实际频率
+#
+#     # 将瞬时频率与时间点拼接起来，作为时间频率幅度谱的一部分
+#     time_freq_amp_spectrum.append(np.vstack((time_points, freq_points)))
+#
+# # 将时间频率幅度谱的各个部分合并为一个二维数组
+# time_freq_amp_spectrum = np.array(time_freq_amp_spectrum)
+#
+# # 绘制时间频率幅度谱
+# plt.pcolormesh(time_freq_amp_spectrum[:, 0, :], time_freq_amp_spectrum[:, 1, :], np.abs(imf))
+# plt.ylabel('Frequency')
+# plt.xlabel('Time')
+# plt.title('Time-Frequency Amplitude Spectrum')
+# plt.colorbar(label='Amplitude')
+# plt.show()
