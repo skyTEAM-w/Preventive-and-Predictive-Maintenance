4c49ba2024afa351f9262583547e9cb896df0e4a
WuChengpei_sky <w2563070231@outlook.com>
2023-11-27 21:49:17 +0800

    CWRU CNN 模型完成

--- src/data/get_batch.py
+++ src/data/get_batch.py
@@ -2,13 +2,6 @@ import numpy as np
 
 
 def get_batch(train_x, train_y, time_step):
-    """
-    LSTM数据集划分方法
-    :param train_x: X
-    :param train_y: Y
-    :param time_step: 时间步长
-    :return: LSTM训练集
-    """
     length = len(train_x) - time_step
     sequence = []
     reserve = []
@@ -16,25 +9,6 @@ def get_batch(train_x, train_y, time_step):
     for i in range(length):
         sequence.append(train_x[i:i + time_step])
         reserve.append(train_y[i:i + time_step])
-
-    return np.array(sequence), np.array(reserve)
-    pass
-
-
-def get_cnn_batch(train_x, train_y):
-    """
-    CNN数据集划分方法
-    :param train_x: X
-    :param train_y: Y
-    :return: CNN训练集
-    """
-    length = len(train_x) - 1
-    sequence = []
-    reserve = []
-
-    for i in range(length):
-        sequence.append(train_x[i])
-        reserve.append(train_y[i])
 
     return np.array(sequence), np.array(reserve)
     pass
